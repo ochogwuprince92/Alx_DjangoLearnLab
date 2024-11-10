24ca2129ae1c4d411a8ca8db957a7b694ab8331f
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-11-10 16:08:28 +0100

    Separate imports for Library and Book in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,15 +2,16 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book
-from .models import Library  # Import both Book and Library models
+from .models import Library 
 
 # Function-based view to list all books
 def list_books(request):
-    books = Book.objects.all()  # Retrieve all books from the database
-    return render(request, 'relationship_app/list_books.html', {'books': books})  # Full path for the template
+    books = Book.objects.all() 
+    context = {'books' : books}
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  # Full path for the template
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
