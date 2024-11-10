a3eecf086863d797d4280c34752e2f8b6b964ae1
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-11-10 20:27:56 +0100

    updated class-based view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.views import View
 from .models import Library, Book
 from django.contrib.auth.decorators import user_passes_test
