c6197499de08337861aeae18838c2d0cfdb15b93
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-11-23 12:29:01 +0100

    Setting Up a New Django Project with Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,8 +3,8 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListAPIView(generics.ListAPIView):
-    queryset = Book.oblects.all()
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 # Create your views here.
