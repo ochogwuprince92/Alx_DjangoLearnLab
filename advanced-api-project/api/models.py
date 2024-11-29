6c896aedebdaf9ed804e6b7ce3c7f620e9b4ac3c
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-11-29 19:25:50 +0100

    Initial commit for advanced_api_project with custom serializers

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
@@ -9,10 +9,9 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name= 'books')
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-
 
 # Create your models here.
