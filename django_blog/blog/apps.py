8f528b1f63d6828358e79ccac986725e1b8c0a71
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-12-08 20:44:58 +0100

    Implementing the Blog's User Authentication System

--- django_blog/blog/apps.py
+++ django_blog/blog/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-class DjangoBlogConfig(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_blog'
+    name = 'blog'
