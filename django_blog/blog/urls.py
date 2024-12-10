85b59d4d8b7172634b0d48254a0f51b78be43491
Ochogwu Prince <ochogwuprince92@gmail.com>
2024-12-10 11:28:31 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("register/", views.register_view, name="register"),
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", views.custom_logout_view, name="logout"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
     path("profile/", views.profile_view, name="profile"),
+    path("home/", views.home, name= "home"),
+    path("post/", views.post, name= "posts"),
 ]
