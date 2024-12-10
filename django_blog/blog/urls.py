d9d40fb56c23d71e82c50bdcee6d57471f12166e
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-12-11 00:52:37 +0100

    Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path("profile/", views.profile_view, name="profile"),
     path("home/", views.home, name= "home"),
     path("post/", views.post, name= "posts"),
+    
+    path('', views.PostListView.as_view(), name='posts'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
 
 def register_view(request):
     if request.method == "POST":
@@ -24,3 +28,50 @@ def home(request):
 def post(request):
     return render(request, 'post.html')
     
+# List View: Display all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+# Detail View: Show individual post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+# Create View: Create a new post
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('posts')
+
+# Update View: Edit an existing post
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def get_queryset(self):
+        # Ensure only the author can edit their posts
+        return Post.objects.filter(author=self.request.user)
+
+    success_url = reverse_lazy('posts')
+
+# Delete View: Delete a post
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        # Ensure only the author can delete their posts
+        return Post.objects.filter(author=self.request.user)
+
+    success_url = reverse_lazy('posts')
