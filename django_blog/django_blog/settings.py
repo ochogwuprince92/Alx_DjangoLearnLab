0e6197a532e9f70717df171be656076c7a06e5ef
Ochogwu Prince <ochogwuprince92@gmail.com>
2024-12-10 11:32:03 +0100

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*^9c4u)esomr_b^h(-$7*ut_x=3*s(jjm3!nxq_lsf^3(_xcj)'
+SECRET_KEY = 'django-insecure-2lo7wil^(ln$@l7wsfzq$0z%2&#vs0-8!!6f-ivfh=7k1ja6gk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'blog/templates/blog'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,15 +76,19 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+      'mypostgress': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_blog',
-        'USER': 'khaneworld',
-        'PASSWORD': 'ogwaa123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
-    }
+        'NAME': 'myDBname',
+        'USER': 'myUser',
+        'PASSWORD': 'myPass',
+        'HOST': '127.0.0.1',
+        'PORT': '8000',
+    },
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -121,10 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS =[
-    BASE_DIR / "blog/static",
-]
+STATICFILES_DIRS = [
+    BASE_DIR / 'blog/static', BASE_DIR / 'static',
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
