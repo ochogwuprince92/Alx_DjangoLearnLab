d0b17197dd45eb68e0bea95d3304a3fe6a76e616
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-12-11 02:42:05 +0100

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post,Tag
 from .models import Comment
+from django.forms import TextInput, Textarea
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -16,6 +17,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+
+    widgets = {
+        'title': forms.TextInput(attrs={'class': 'form-control'}),
+        'content': forms.Textarea(attrs={'class': 'form-control'}),
+}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
