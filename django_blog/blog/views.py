1bb90eeab9b733aec4918b06281334831535eee6
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-12-11 01:17:51 +0100

    another commit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -28,25 +28,50 @@ def home(request):
 def post(request):
     return render(request, 'post.html')
     
+# List View: Display all posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
 
+# Detail View: Show individual post
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
 
+# Create View: Create a new post
 class PostCreateView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'blog/post_form.html'
-    fields = ['title', 'content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('posts')
+
+# Update View: Edit an existing post
 class PostUpdateView(UpdateView):
     model = Post
+    form_class = PostForm
     template_name = 'blog/post_form.html'
-    fields = ['title', 'content']
 
+    def get_queryset(self):
+        # Ensure only the author can edit their posts
+        return Post.objects.filter(author=self.request.user)
+
+    success_url = reverse_lazy('posts')
+
+# Delete View: Delete a post
 class PostDeleteView(DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post_list')
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        # Ensure only the author can delete their posts
+        return Post.objects.filter(author=self.request.user)
+
+    success_url = reverse_lazy('posts')
