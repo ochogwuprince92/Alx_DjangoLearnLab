707c291bcc356835e5788edf994de199957dd57a
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-12-14 11:19:31 +0100

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,34 @@
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
+# Get the user model
 User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True)
-    
+    password = serializers.CharField(write_only=True)  # Use CharField explicitly for password
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture', 'followers']
 
     def create(self, validated_data):
+        """
+        Create a new user instance with a hashed password
+        """
+        # Use create_user to handle password hashing
         user = User.objects.create_user(
-        username=validated_data['username'],
-        email=validated_data.get('email'),
-        password=validated_data['password'],
-        bio=validated_data.get('bio'),
-        profile_picture=validated_data.get('profile_picture'),    
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
         )
+
+        # Additional fields
+        user.bio = validated_data.get('bio', '')
+        user.profile_picture = validated_data.get('profile_picture', None)
+        user.save()
+
+        # Create a token for the user
         Token.objects.create(user=user)
+
         return user
