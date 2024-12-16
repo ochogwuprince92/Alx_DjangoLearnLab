e1d15b8a724ac7045cf3677e07592ddd7acc1fea
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-12-16 01:47:16 +0100

    another commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,49 +2,35 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-# Get the user model
 User = get_user_model()
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)  # Use CharField explicitly for password
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    bio = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+        fields = ['username', 'email', 'password', 'bio']
 
     def create(self, validated_data):
-        """
-        Create a new user instance with a hashed password
-        """
-        # Use create_user to handle password hashing
-        user = get_user_model().objects.create_user(
+        user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password']
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
         )
-
-        # Additional fields
-        user.bio = validated_data.get('bio', '')
-        user.profile_picture = validated_data.get('profile_picture', None)
-        user.save()
-
-        # Create a token for the user
-        Token.objects.create(user=user)
-
         return user
-from rest_framework import serializers
-from .models import Post, Comment
 
-class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
 
-    class Meta:
-        model = Post
-        fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
-
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = Comment
-        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
+        fields = ['username', 'password']
+
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ['key', 'user']
